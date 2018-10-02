8e7c72c4fd0807e42d70809b7128bfcfd9d23c2d
Juan Añón <janon@plainconcepts.com>
2018-10-02 13:00:18 +0200

    Added test scaffolding.

---
+++ src/BASE.Tests/Core/BBVA/ClientPreferencesTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BASE.Tests.Core.BBVA
+{
+    public class ClientPreferencesTests
+    {
+        [TestClass]
+        public class UnitTest1
+        {
+            [TestMethod]
+            public void Should_be_return_a_valid_client_preferences()
+            {
+            }
+        }
+    }
+}
