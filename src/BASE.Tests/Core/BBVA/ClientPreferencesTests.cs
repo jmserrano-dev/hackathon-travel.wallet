52228a718002399042cc1880def653a9bb6210b7
Juan Añón <janon@plainconcepts.com>
2018-10-02 13:04:09 +0200

    Fixex bad naming.

--- src/BASE.Tests/Core/BBVA/ClientPreferencesTests.cs
+++ src/BASE.Tests/Core/BBVA/ClientPreferencesTests.cs
@@ -4,13 +4,10 @@ namespace BASE.Tests.Core.BBVA
 {
     public class ClientPreferencesTests
     {
-        [TestClass]
-        public class UnitTest1
+        [TestMethod]
+        public void Should_be_return_a_valid_client_preferences()
         {
-            [TestMethod]
-            public void Should_be_return_a_valid_client_preferences()
-            {
-            }
         }
     }
+
 }
