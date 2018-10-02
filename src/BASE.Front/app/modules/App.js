e79610fbc4720fe7dbbb340a581e6874e0d243fd
Eva Crespo <ecrespo@plainconcepts.com>
2018-10-02 18:06:53 +0200

    router

--- src/BASE.Front/app/modules/App.js
+++ src/BASE.Front/app/modules/App.js
@@ -3,9 +3,11 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import HeaderApp from './components/Shared/Header/Header';
 import Pallete from '../shared/MaterialTheme/MaterialTheme';
 import Home from './components/Home/Home';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import '@styles/import.scss';
 import {} from './app.scss';
+import Recommendations from './components/Recommendations/Recommendations';
 
 export class App extends Component {
   render() {
@@ -15,8 +17,17 @@ export class App extends Component {
           <HeaderApp />
         </header>
         <main>
-            <Home></Home>
-            </main>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route
+                exact
+                path="/recommendations"
+                component={Recommendations}
+              />
+            </Switch>
+          </BrowserRouter>
+        </main>
       </MuiThemeProvider>
     );
   }
--- src/BASE.Front/app/modules/components/Home/Home.js
+++ src/BASE.Front/app/modules/components/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import classes from './Home.scss';
 import card from 'assets/images/Group.png';
+import travel from 'assets/images/tickets.png';
+import travelAdv from 'assets/images/tickets 2.png';
 import AddIcon from '@material-ui/icons/Add';
 
 const Home = () => {
@@ -13,9 +15,22 @@ const Home = () => {
           <AddIcon />
         </div>
         <div className={classes.cardsWrapper}>
-          <img src={card} className="mr-2" />{' '}
-          <img src={card} className="mr-2" />{' '}
           <img src={card} className="mr-2" />
+          <img src={card} className="mr-2" />
+          <img src={card} className="mr-2" />
+        </div>
+      </div>
+      <div className={classes.sectionWrapper}>
+        <div className={classes.titleWrapper}>
+          <h2>Travels</h2>
+        </div>
+        <div className={classes.travelWrapper}>
+          <Link to="/recommendations">
+            <img src={travel} className="mb-2" />
+          </Link>
+          <Link to="/recommendations">
+            <img src={travelAdv} className="mb-2" />
+          </Link>
         </div>
       </div>
     </div>
