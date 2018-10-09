f6d553674e349d589f8e51207b9bf3459839d6cd
José Manuel <jmserrano.dev@outlook.com>
2018-10-09 12:22:30 +0200

    Clean

--- src/BASE.Tests/Core/BBVA/ClientPreferencesTests.cs
+++ src/BASE.Tests/Core/BBVA/ClientPreferencesTests.cs
@@ -10,7 +10,7 @@ namespace BASE.Tests.Core.BBVA
         [TestMethod]
         public void Should_be_return_a_valid_client_preferences()
         {
-            const string token = "YXBwLmJidmEucGxhaW46VkFLUXQwNHRzb2FQb0ZVQGw5Q09KODQlV3ZCV09BVGM0dGVLekZneVo5endAakdDdnhsRVZBaFdBR2pKVFlTWQ==";
+            const string token = "TOKEN";
             var client = new BBVAuthClient(token);
             var result = client.GetCredentials().Result;
 
